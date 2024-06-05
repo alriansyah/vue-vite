3f3df7b3dee8aa6dac1e3c0a71df6e64dd6ad002
alriansyah <alriansyah1218@gmail.com>
2024-06-05 17:49:01 +0700

    feat: add state and set state

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,36 +1,33 @@
 <script setup>
-import { ref } from 'vue'
+import { ref } from "vue";
 
 defineProps({
   msg: String,
-})
+});
 
-const count = ref(0)
+const count = ref(0);
+
+const decrement = () => {
+  if (count.value > 0) {
+    count.value--;
+  }
+};
+
+const increment = () => {
+  count.value++;
+};
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+    <div class="card-content">{{ count }}</div>
+    <div class="card-actions">
+      <button type="button" @click="decrement">-</button>
+      <button type="button" @click="increment">+</button>
+    </div>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
 <style scoped>
