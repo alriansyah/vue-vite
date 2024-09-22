3c6640ae9e8e2bca07e2e29dceb1b339b44e4a08
alriansyah <alriansyah1218@gmail.com>
2024-09-22 21:09:08 +0700

    feat: learn vue Membuat Ui Bagian Memo

--- src/components/StickyNote/IndexStickyNote.vue
+++ src/components/StickyNote/IndexStickyNote.vue
@@ -7,7 +7,29 @@
       <button class="header-button" type="button">+</button>
     </header>
     <main>
-        <div>[card]</div>
+      <div class="card-container">
+        <div class="card">
+          <p class="card-content">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum eius iste corporis, fugiat deleniti
+            maxime. Repudiandae, ipsa. Sunt, itaque illum?
+          </p>
+          <p>22/09/2024</p>
+        </div>
+        <div class="card">
+          <p class="card-content">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum eius iste corporis, fugiat deleniti
+            maxime. Repudiandae, ipsa. Sunt, itaque illum?
+          </p>
+          <p>22/09/2024</p>
+        </div>
+        <div class="card">
+          <p class="card-content">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum eius iste corporis, fugiat deleniti
+            maxime. Repudiandae, ipsa. Sunt, itaque illum?
+          </p>
+          <p>22/09/2024</p>
+        </div>
+      </div>
     </main>
   </div>
 </template>
@@ -25,26 +47,44 @@
 }
 
 header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 
 .header-title {
-    font-size: 48px;
-    font-weight: bold;
-    margin-bottom: 25px;
-    color: #495a7d;
+  font-size: 48px;
+  font-weight: bold;
+  margin-bottom: 25px;
+  color: #495a7d;
 }
 
 .header-button {
-    border: none;
-    padding: 10px;
-    width: 50px;
-    height: 50px;
-    cursor: pointer;
-    border-radius: 100%;
-    background-color: #495a7d;
-    color: #fff;
+  border: none;
+  padding: 10px;
+  width: 50px;
+  height: 50px;
+  cursor: pointer;
+  border-radius: 100%;
+  background-color: #495a7d;
+  color: #fff;
+}
+
+.card-container {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+}
+
+.card {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 225px;
+  height: 225px;
+  padding: 10px;
+  background-color: #ffa6c1;
+  /* border-radius: ; */
+  margin-bottom: 20px;
 }
 </style>
